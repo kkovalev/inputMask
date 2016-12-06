49ceabc7726cf5d450f2f518053251fac1d15c05
Kirill <seokirillstarsnab@gmail.com>
2016-12-06 22:30:44 +0300

    no message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 input.addEventListener('input', function(){
-    if(this.matches(':invalid')) this.value = this.prevValue || '';
+    if(this.validity.patternMismatch) this.value = this.prevValue || '';
     this.prevValue = this.value;
 });
